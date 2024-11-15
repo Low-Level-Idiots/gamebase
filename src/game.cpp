3145e24bd89c27f4971999de4a47b7c390d55166
sudotto <codekiwi132@gmail.com>
2024-11-15 11:09:56 -0800

    worked on main

--- src/game.cpp
+++ src/game.cpp
@@ -7,38 +7,37 @@
 #include "img.h"
 
 Game::Game(std::string title, int x, int y, int w, int h, int frame_rate){
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Init(SDL_INIT_EVERYTHING);                                                                 // init lib
 	IMG_Init(IMG_INIT_PNG);
 	win = SDL_CreateWindow(title.c_str(), x, y, w, h, 0);                                          // create the window
 	rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);                                  // create renderer
-
-	Img icon(rend, "assets/icon.png");
-	icon.set_icon(win);
-
-	fps = frame_rate;
-	running = true;
+	Img icon(rend, "assets/icon.png");                                                             // load icon.png as img
+	icon.set_icon(win);                                                                            // set icon.png as window icon
+	fps = frame_rate;                                                                              // store max fps
+	running = true;                                                                                // set main loop flag
 }
 
 void Game::frame_cap(Uint32 ticks){
-	if(ticks < 1/fps){
-		SDL_Delay(1/fps - ticks);
+	if(ticks < 1/fps){                                                                             // if cycle time is less than framerate in ticks...
+		SDL_Delay(1/fps - ticks);                                                                  // halt until cycle has taken correct amount of time
 	}
 }
 
 void Game::main(){
-	SDL_Event event;
-	while(running){
-		Uint32 start = SDL_GetTicks();
-		while(SDL_PollEvent(&event)){
-			events.push_back(event);
+	SDL_Event event;                                                                               // declare event
+	while(running){                                                                                // while the game is running...
+		Uint32 start = SDL_GetTicks();                                                             // get current time in ticks
+		events.clear();                                                                            // reset event list
+		while(SDL_PollEvent(&event)){                                                              // while events are getting gotten...
+			events.push_back(event);                                                               // push event gotten to list of events
 		}
-		for(int i = 0; i < events.size(); i++){
-			switch(events[i].type){
-				case SDL_QUIT:
-					running = false;
+		for(int i = 0; i < events.size(); i++){                                                    // iterate through events
+			switch(events[i].type){                                                                // checking type of event...
+				case SDL_QUIT:                                                                     // if type is quit
+					running = false;                                                               // kill game
 					break;
 			}
 		}
-		frame_cap(start - SDL_GetTicks());
+		frame_cap(start - SDL_GetTicks());                                                         // cap frame rate
 	}
 }
--- src/game.h
+++ src/game.h
@@ -7,13 +7,13 @@
 
 class Game{
 	public:
-		Game(std::string title, int x, int y, int w, int h, int frame_rate);
-		SDL_Window* win;
-		SDL_Renderer* rend;
-		int fps;
-		std::vector<SDL_Event> events;
-		bool running;
+		Game(std::string title, int x, int y, int w, int h, int frame_rate);                       // constructor
+		SDL_Window* win;                                                                           // game window
+		SDL_Renderer* rend;                                                                        // renderer for graphics
+		int fps;                                                                                   // max framerate for game
+		std::vector<SDL_Event> events;                                                             // list of events for handling
+		bool running;                                                                              // game running flag
 	public:
-		void frame_cap(Uint32 ticks);
-		void main();
+		void frame_cap(Uint32 ticks);                                                              // cap framerate for performance
+		void main();                                                                               // main loop
 };
