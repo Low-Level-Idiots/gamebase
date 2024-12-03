9458ab58520ecc5e60957032478c2fc6d64fb3bf
sudotto <codekiwi132@gmail.com>
2024-12-03 10:01:03 -0800

    worked on ui ill finish later

--- src/ui.cpp
+++ src/ui.cpp
@@ -19,3 +19,30 @@ void Img::render(SDL_Renderer* rend, int x, int y, int w, int h){
 void Img::set_icon(SDL_Window* win){
 	SDL_SetWindowIcon(win, surf);
 }
+
+// RECT MATH
+
+Rect::Rect(int x_, int y_, int w_, int h_){
+	x = x_;
+	y = y_;
+	w = w_;
+	h = h_;
+}
+
+bool Rect::colliding(int x_, int y_, int w_, int h_){                          // check if this rect is colliding with another rectangle specified
+	
+}
+
+// BUTTONS
+
+Button::Button(Img img, Rect rect){                                            // construct a button object
+	
+}
+
+bool Button::hover(int mouse_x, int mouse_y){
+	
+}
+
+bool Button::clicked(int mouse_x, int mouse_y, std::vector<SDL_Event> events){
+	
+}
--- src/ui.h
+++ src/ui.h
@@ -9,8 +9,29 @@
 
 class Img {
 	public:
-		Img(std::string file);
-		SDL_Surface* surf;
-		void render(SDL_Renderer* rend, int x, int y, int w, int h);
-		void set_icon(SDL_Window* win);
+		Img(std::string file);                                                 // construct an image object
+		SDL_Surface* surf;                                                     // SDL surface for image
+		void render(SDL_Renderer* rend, int x, int y, int w, int h);           // function to draw the image to the screen
+		void set_icon(SDL_Window* win);                                        // set the image as the window icon
+};
+
+// RECTANGLE CLASS
+
+class Rect {
+	public:
+		Rect(int x, int y, int w, int h);                                      // construct a rectangle object
+		int x;
+		int y;
+		int w;
+		int h;
+		bool colliding(int x, int y, int w, int h);                            // check if this rect is colliding with another rectangle specified
+};
+
+// BUTTON CLASS
+
+class Button {
+	public:
+		Button(Img img, Rect rect);                                            // construct a button object
+		bool hover(int mouse_x, int mouse_y);
+		bool clicked(int mouse_x, int mouse_y, std::vector<SDL_Event> events);
 };
